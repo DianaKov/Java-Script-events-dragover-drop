31fa3fe912787922961de4d2502ba010d4bde6a4
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-23 10:39:11 +0200

    Update app.js

--- Football field/app.js
+++ Football field/app.js
@@ -1,12 +1,12 @@
 const img = document.querySelector('.poligon__ball');
 const poligon = document.querySelector('.poligon');
 const sound = new Audio('./img/tolpa-na-stadione-ozhidanie-svist-vykrikpi-i-obshhij-privetstvennyj-hor-na-vyhod.mp3');
+const hiddenImgRight = document.querySelector('.poligon__hidden-right');
+const hiddenImgLeft = document.querySelector('.poligon__hidden-left');
 let guest = document.querySelector('.scoreboard__guest-points'); 
 let host = document.querySelector('.scoreboard__host-points');
 let countGuest = 0; 
 let countHost = 0;
-const hiddenImgRight = document.querySelector('.poligon__hidden-right');
-const hiddenImgLeft = document.querySelector('.poligon__hidden-left');
 
 poligon.addEventListener('dragover', function(e){
     e.preventDefault();
@@ -16,7 +16,7 @@ poligon.addEventListener('drop', function(e){
 
     this.appendChild(img);
     img.style.left = e.clientX - img.offsetWidth / 2 + 'px';
-			img.style.top = e.clientY - img.offsetHeight / 2 + 'px';
+    img.style.top = e.clientY - img.offsetHeight / 2 + 'px';
 
 
    /* this.appendChild(img);
