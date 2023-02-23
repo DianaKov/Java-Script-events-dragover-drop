5902d340653a781bce42665b2ffec7461e102b91
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-23 13:37:34 +0200

    Update app.js

--- Football field/app.js
+++ Football field/app.js
@@ -1,12 +1,12 @@
 const img = document.querySelector('.poligon__ball');
 const poligon = document.querySelector('.poligon');
 const sound = new Audio('./img/tolpa-na-stadione-ozhidanie-svist-vykrikpi-i-obshhij-privetstvennyj-hor-na-vyhod.mp3');
-const hiddenImgRight = document.querySelector('.poligon__hidden-right');
-const hiddenImgLeft = document.querySelector('.poligon__hidden-left');
 let guest = document.querySelector('.scoreboard__guest-points'); 
 let host = document.querySelector('.scoreboard__host-points');
 let countGuest = 0; 
 let countHost = 0;
+const hiddenImgRight = document.querySelector('.poligon__hidden-right');
+const hiddenImgLeft = document.querySelector('.poligon__hidden-left');
 
 poligon.addEventListener('dragover', function(e){
     e.preventDefault();
@@ -16,7 +16,7 @@ poligon.addEventListener('drop', function(e){
 
     this.appendChild(img);
     img.style.left = e.clientX - img.offsetWidth / 2 + 'px';
-    img.style.top = e.clientY - img.offsetHeight / 2 + 'px';
+	img.style.top = e.clientY - img.offsetHeight / 2 + 'px';
 
 
    /* this.appendChild(img);
@@ -24,7 +24,8 @@ poligon.addEventListener('drop', function(e){
 	img.style.top = e.clientY + 'px';*/
 
     if (e.target.classList.contains('poligon__first')) {
-        addPoints(hiddenImgRight, guest, 'scoreboard__guest-points');
+        console.log(1);
+                addPoints(hiddenImgRight, guest, 'scoreboard__guest-points');
         countGuest++; 
         guest.textContent = countGuest;
     }
@@ -46,11 +47,9 @@ poligon.addEventListener('drop', function(e){
     }
 
     function addImg(a){
-        a.style.display = 'block';
+        a.style.visibility = 'visible';
         setTimeout(() => {
-            a.style.display = 'none';
+            a.style.visibility = 'hidden';
         }, 3000);
     }
 });
-
-   
